3604afef3778dfe8602cbc3b3571b6fe738b4816
Joseph Burnett <josephburnett79@gmail.com>
2018-04-07 17:38:34 -0400

    Simplify example.

--- example/hello.go
+++ example/hello.go
@@ -7,8 +7,7 @@ import (
 	"github.com/josephburnett/k8sflag/pkg/k8sflag"
 )
 
-var config = k8sflag.NewConfigMap("/etc/config")
-var name = config.String("hello.name", "nobody")
+var name = k8sflag.String("hello.name", "nobody")
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello %v.\n", name.Get())
